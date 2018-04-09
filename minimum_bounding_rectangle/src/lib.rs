efa0126cf1c0691c6c030bc7cb47ceaa2f149480
ToF <cthibaut@octo.com>
2018-04-09 15:14:43 +0200

    vec min coord, refactoring

--- minimum_bounding_rectangle/src/lib.rs
+++ minimum_bounding_rectangle/src/lib.rs
@@ -2,87 +2,87 @@
 mod tests {
     use std::cmp;
 
-    struct Coords {
+    struct Point {
         x:i32, y:i32 
     }
     enum Shape {
-        Point  { coords : Coords },
-        Line   { coords0 : Coords, coords1 : Coords },
-        Circle { coords : Coords, radius : i32 },
+        Point  { point : Point },  
+        Line   { point0 : Point, point1 : Point },
+        Circle { point : Point, radius : i32 },
     }
 
     impl Shape {
-        fn ext_coords(self, extremum:&Fn(i32,i32) -> i32) -> Coords {
+        fn ext_point(self, extremum:&Fn(i32,i32) -> i32) -> Point {
             match self {
-                Shape::Point { coords } => coords, 
-                Shape::Line { coords0, coords1 } => Coords { 
-                    x:extremum(coords0.x, coords1.x), 
-                    y:extremum(coords0.y, coords1.y) },
-                Shape::Circle { coords, radius } => Coords {
-                    x: extremum(coords.x - radius, coords.x + radius),
-                    y: extremum(coords.y - radius, coords.y + radius) },
+                Shape::Point{ point } => point,
+                Shape::Line { point0, point1 } => Point { 
+                    x:extremum(point0.x, point1.x), 
+                    y:extremum(point0.y, point1.y) },
+                Shape::Circle { point, radius } => Point {
+                    x: extremum(point.x - radius, point.x + radius),
+                    y: extremum(point.y - radius, point.y + radius) },
             }
         }
 
-        fn min_coords(self) -> Coords {
-            self.ext_coords(&cmp::min)
+        fn min_point(self) -> Point {
+            self.ext_point(&cmp::min)
         }
-        fn max_coords(self) -> Coords {
-            self.ext_coords(&cmp::max)
+        fn max_point(self) -> Point {
+            self.ext_point(&cmp::max)
         }
     }
-    fn min_coords(v : Vec<Shape>) -> Coords {
-        let initial = Shape::Point { coords :Coords { x:i32::max_value(), y:i32::max_value() }};
-        initial.min_coords()
+    fn min_point(v : Vec<Shape>) -> Point {
+        let initial = Shape::Point { point :Point { x:i32::max_value(), y:i32::max_value() }};
+        initial.min_point()
     }
     #[test]
-    fn point_min_coords_test() {
-        let p = Shape::Point { coords : Coords { x:-3, y:5 }};
-        let c = p.min_coords();
+    fn point_min_point_test() {
+        let p = Shape::Point { point : Point { x:-3, y:5 }};
+        let c = p.min_point();
         assert_eq!(c.x, -3);
         assert_eq!(c.y, 5)
     }
     #[test]
-    fn line_min_coords_test() {
-        let p = Shape::Line { coords0 : Coords { x:-3, y:5 }, coords1 : Coords { x:4, y:-25} };
-        let c = p.min_coords();
+    fn line_min_point_test() {
+        let p = Shape::Line { point0 : Point { x:-3, y:5 }, point1 : Point { x:4, y:-25} };
+        let c = p.min_point();
         assert_eq!(c.x, -3);
         assert_eq!(c.y, -25)
     }
     #[test]
-    fn circle_min_coords_test() {
-        let p = Shape::Circle { coords : Coords { x:-3, y:5 }, radius : 10 };
-        let c = p.min_coords();
+    fn circle_min_point_test() {
+        let p = Shape::Circle { point : Point { x:-3, y:5 }, radius : 10 };
+        let c = p.min_point();
         assert_eq!(c.x, -13);
         assert_eq!(c.y, -5)
     }
     #[test]
-    fn point_max_coords_test() {
-        let p = Shape::Point { coords : Coords { x:-3, y:5 }};
-        let c = p.max_coords();
+    fn point_max_point_test() {
+        let p = Shape::Point { point : Point { x:-3, y:5 }};
+        let c = p.max_point();
         assert_eq!(c.x, -3);
         assert_eq!(c.y, 5)
     }
     #[test]
-    fn line_max_coords_test() {
-        let p = Shape::Line { coords0 : Coords { x:-3, y:5 }, coords1 : Coords { x:4, y:-25} };
-        let c = p.max_coords();
+    fn line_max_point_test() {
+        let p = Shape::Line { point0 : Point { x:-3, y:5 }, point1 : Point { x:4, y:-25} };
+        let c = p.max_point();
         assert_eq!(c.x, 4);
         assert_eq!(c.y, 5)
     }
     #[test]
-    fn circle_max_coords_test() {
-        let p = Shape::Circle { coords : Coords { x:-3, y:5 }, radius : 10 };
-        let c = p.max_coords();
+    fn circle_max_point_test() {
+        let p = Shape::Circle { point : Point { x:-3, y:5 }, radius : 10 };
+        let c = p.max_point();
         assert_eq!(c.x, 7);
         assert_eq!(c.y, 15)
     }
     #[test]
-    fn min_coords_of_a_list_of_shapes() {
-        let v = vec![Shape::Point { coords : Coords { x:-3, y:5}}
-                    ,Shape::Line { coords0 : Coords { x:-3, y:5 }, coords1 : Coords { x:4, y:-25} }
-                    ,Shape::Circle { coords : Coords { x:-3, y:5 }, radius : 10 }];
-        let c = min_coords(v);
+    fn min_point_of_a_list_of_shapes() {
+        let v = vec![Shape::Point { point : Point { x:-3, y:5}}
+                    ,Shape::Line { point0 : Point { x:-3, y:5 }, point1 : Point { x:4, y:-25} }
+                    ,Shape::Circle { point : Point { x:-3, y:5 }, radius : 10 }];
+        let c = min_point(v);
         assert_eq!(c.x,0);
 
     }

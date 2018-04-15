d74506d64c55cbfbeca90bbc2b25a06a419b89eb
ToF <cthibaut@octo.com>
2018-04-15 10:49:25 +0200

    char patterns 8

---
+++ char_patterns_8/target/debug/char_patterns_8.d
@@ -0,0 +1,1 @@
+/Users/tof/dev/LearningRust/char_patterns_8/target/debug/char_patterns_8: /Users/tof/dev/LearningRust/char_patterns_8/src/lib.rs /Users/tof/dev/LearningRust/char_patterns_8/src/main.rs
---
+++ char_patterns_8/target/debug/libchar_patterns_8.d
@@ -0,0 +1,1 @@
+/Users/tof/dev/LearningRust/char_patterns_8/target/debug/libchar_patterns_8.rlib: /Users/tof/dev/LearningRust/char_patterns_8/src/lib.rs

2579288700c3ae77eeeb0f00ca3a22b7765b3412
ToF- <cthibaut@octo.com>
2018-05-02 18:45:33 +0200

    fun with sequence (blocked on for_each)

---
+++ fun_with_sequences/target/debug/fun_with_sequences.d
@@ -0,0 +1,1 @@
+/Users/Christophe/dev/LearningRust/fun_with_sequences/target/debug/fun_with_sequences: /Users/Christophe/dev/LearningRust/fun_with_sequences/src/main.rs /Users/Christophe/dev/LearningRust/fun_with_sequences/src/lib.rs
